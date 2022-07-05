727b807ec4c09f87bbd18570d1c027796edd047a
Thanasak <bestzacap20@gmail.com>
2022-07-05 09:57:13 +0700

    add 0 to hours if it only 1 digit

--- components/CurrentTime.js
+++ components/CurrentTime.js
@@ -4,8 +4,8 @@ function CurrentTime() {
 
   const getTimeFormat = () => {
     const dateTime = new Date
-    const getHours = dateTime.getHours()
-    const getMinutes = String(dateTime.getMinutes()).padStart(2, "0");
+    const getHours = String(dateTime.getHours()).padStart(2, "0")
+    const getMinutes = String(dateTime.getMinutes()).padStart(2, "0")
     const getDate = dateTime.getDate()
     const getDay = dateTime.toLocaleString('en-us', {  weekday: 'long' })
     const getYear = dateTime.getFullYear()
