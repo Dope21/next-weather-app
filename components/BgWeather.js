2fd26166f184b4ea21bdff1dedd41992e2e3fcd0
Thanasak <bestzacap20@gmail.com>
2022-07-12 12:02:52 +0700

    fix(background): prettier

--- components/BgWeather.js
+++ components/BgWeather.js
@@ -1,22 +1,17 @@
 import Image from 'next/image'
 import callBackground from '../utils/callBackground'
 
-export default function BgWeather({
-  weather,
-  time
-}) {
+export default function BgWeather({ weather, time }) {
   const timePhase = JSON.stringify(time).charAt(3)
 
   return (
-    <div 
-      className='absolute inset-0 transition-all'
-    >
-      <div className='absolute w-full h-full bg-black opacity-50 z-10' />      
-      <Image 
+    <div className="absolute inset-0 transition-all">
+      <div className="absolute w-full h-full bg-black opacity-50 z-10" />
+      <Image
         src={callBackground(weather, timePhase)}
-        alt='weather'
-        layout='fill'
-        className='object-cover z-0'
+        alt="weather"
+        layout="fill"
+        className="object-cover z-0"
         priority
       />
     </div>
